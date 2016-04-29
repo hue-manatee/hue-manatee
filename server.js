ff4c2a0f693c031bb3a78a96b1afe6a8e4915a9c
bonecrushereb <bonecrushereb@gmail.com>
2016-04-29 10:53:39 -0700

    add 404 message to server.js closes #35

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 
-
+app.use((req, res) => {
+  res.status(404).send('Page not found!');
+});
 module.exports = app.listen(PORT, () => console.log('server up on port:' + PORT));
