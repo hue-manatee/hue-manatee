56f65ae2712ef9ba4e68685bfbea78cef09534f3
bharding2 <b.harding@gmail.com>
2016-04-29 10:33:29 -0700

    gulpfile update closes #28

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,32 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 
-var appFiles = [];
-var testFiles = [];
+// initial app directories, will need to add more as repo expands
+var appFiles = ['*.js', './lib/**/*.js', './routes/**/*.js', './models/**/*.js'];
+var testFiles = ['./test/**/*.js'];
+
+gulp.task('test:mocha', () => {
+  return gulp.src(testFiles)
+    .pipe(mocha());
+});
+
+gulp.task('lint:testFiles', () => {
+  return gulp.src(testFiles)
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('lint:appFiles', () => {
+  return gulp.src(appFiles)
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('test', ['test:mocha']);
+gulp.task('lint', ['lint:testFiles', 'lint:appFiles']);
+gulp.task('watch', () => {
+  gulp.watch(testFiles, ['test:mocha', 'lint:testFiles']);
+  gulp.watch(appFiles, ['test:mocha', 'lint:appFiles']);
+});
+
+gulp.task('default', ['watch', 'lint', 'test']);
