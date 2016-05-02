736d110edb95ebd5e31b6c4e066307003cce53cd
BRANDON PARKER <Brandon@dkparker.com>
2016-05-02 10:01:17 -0700

    fixed Carrie's typo. Closes #19

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bctrypt');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 var userSchema = new mongoose.Schema({
