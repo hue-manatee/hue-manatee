cd2b2c8447d0b406b3365bab77dc81149f8f0ca9
CarrieShort <carrie.r.phillips@gmail.com>
2016-05-02 13:49:29 -0700

    add auth to bridge routers and added extra error handling closes #54

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const authRouter = require(__dirname + '/routes/auth_router');
+const bridgeRouter = require(__dirname + '/routes/bridge_router');
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/hue_db');
 
 app.use('/api', authRouter);
+app.use('/api', bridgeRouter);
+
 
 app.use((req, res) => {
   res.status(404).send('Page not found!');
