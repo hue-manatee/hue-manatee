b95fea24e83b1ba84c62e3e6b4fc8d93a38ff320
CarrieShort <carrie.r.phillips@gmail.com>
2016-05-04 13:49:38 -0700

    add tests to check auth validation of username and password closes #101

--- test/auth_test.js
+++ test/auth_test.js
@@ -58,4 +58,76 @@ describe('the login route tests', () => {
         done();
       });
   });
+  it('should fail to create a user when name is less than 8 characters', (done) => {
+    request('localhost:' + port)
+      .post('/api/signup')
+      .send({ username: 'fail8', password: 'Test23456' })
+      .end((err, res) => {
+        expect(err.toString()).to.eql('Error: Internal Server Error');
+        expect(res.status).to.eql(500);
+        expect(res.body.msg).to.eql('username must be 8 characters long');
+        done();
+      });
+  });
+  it('should fail to create a user when name is less than 8 characters', (done) => {
+    request('localhost:' + port)
+      .post('/api/signup')
+      .send({ username: 'failevenmorecuzitslongerthan24', password: 'Test23456' })
+      .end((err, res) => {
+        expect(err.toString()).to.eql('Error: Internal Server Error');
+        expect(res.status).to.eql(500);
+        expect(res.body.msg).to.eql('username must be 24 characters long');
+        done();
+      });
+  });
+  it('should fail to create a user when password is less than 8 characters', (done) => {
+    request('localhost:' + port)
+      .post('/api/signup')
+      .send({ username: 'imagooduser', password: 'badpass' })
+      .end((err, res) => {
+        expect(err.toString()).to.eql('Error: Internal Server Error');
+        expect(res.status).to.eql(500);
+        expect(res.body.msg).to.eql('password must be 8 characters long');
+        done();
+      });
+  });
+  it('should fail to create a user when password does not contain number',
+  (done) => {
+    request('localhost:' + port)
+      .post('/api/signup')
+      .send({ username: 'imagooduser', password: 'mostlygGood' })
+      .end((err, res) => {
+        expect(err.toString()).to.eql('Error: Internal Server Error');
+        expect(res.status).to.eql(500);
+        expect(res.body.msg)
+        .to.eql('password needs uppercase and lowercase letters and at least one number');
+        done();
+      });
+  });
+  it('should fail to create a user when password does not contain an uppercase letter',
+  (done) => {
+    request('localhost:' + port)
+      .post('/api/signup')
+      .send({ username: 'imagooduser', password: 'mostlyggood1' })
+      .end((err, res) => {
+        expect(err.toString()).to.eql('Error: Internal Server Error');
+        expect(res.status).to.eql(500);
+        expect(res.body.msg)
+        .to.eql('password needs uppercase and lowercase letters and at least one number');
+        done();
+      });
+  });
+  it('should fail to create a user when password does not contain an lowercase letter',
+  (done) => {
+    request('localhost:' + port)
+      .post('/api/signup')
+      .send({ username: 'imagooduser', password: 'MOSTLYGOOD1' })
+      .end((err, res) => {
+        expect(err.toString()).to.eql('Error: Internal Server Error');
+        expect(res.status).to.eql(500);
+        expect(res.body.msg)
+        .to.eql('password needs uppercase and lowercase letters and at least one number');
+        done();
+      });
+  });
 });
