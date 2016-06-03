ec531f5b1731d39c1ca5beddd9c384ce6addbb15
bonecrushereb <bonecrushereb@gmail.com>
2016-06-03 12:40:51 -0700

    add build:dev to gulp closes #140

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,37 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
+const webpack = require('webpack-stream');
 
 var appFiles = ['*.js', './lib/**/*.js', './routes/**/*.js', './models/**/*.js'];
 var testFiles = ['./test/**/*.js'];
+
+gulp.task('webpack:dev', () => {
+  gulp.src('app/js/entry.js')
+    .pipe(webpack({
+      devtool: 'source-map',
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('webpack:test', () => {
+  gulp.src('test/unit/test_entry.js')
+   .pipe(webpack({
+     devtool: 'source-map',
+     output: {
+       filename: 'bundle.js'
+     }
+   }))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('static:dev', ['webpack:dev'], () => {
+  gulp.src(['app/**/*.html'])
+   .pipe(gulp.dest('./build'));
+});
 
 gulp.task('test:mocha', () => {
   return gulp.src(testFiles)
@@ -22,6 +50,7 @@ gulp.task('lint:appFiles', () => {
     .pipe(eslint.format());
 });
 
+gulp.task('build:dev', ['webpack:dev', 'static:dev']);
 gulp.task('test', ['test:mocha']);
 gulp.task('lint', ['lint:testFiles', 'lint:appFiles']);
 
