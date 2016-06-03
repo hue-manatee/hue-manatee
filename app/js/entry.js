46c92ed0ad05513404486cf9f2ec349aff8841dd
BRANDON PARKER <Brandon@dkparker.com>
2016-06-03 14:59:59 -0700

    set up routing and auth template, updated index html file

--- app/js/entry.js
+++ app/js/entry.js
@@ -1,6 +1,34 @@
 const angular = require('angular');
 const hueApp = angular.module('hueApp', [require('angular-route')]);
 
-require('./controllers')(hueApp);
-require('./directives')(hueApp);
-require('./services')(hueApp);
+// require('./controllers')(hueApp);
+// require('./directives')(hueApp);
+// require('./services')(hueApp);
+
+hueApp.config(['$routeProvider', function($routing) {
+  $routing
+    .when('/dashboard', {
+      templateUrl: 'templates/views/dashboard_view.html'
+    })
+    .when('/bridge', {
+      templateUrl: 'templates/views/bridge_view.html'
+    })
+    .when('/group', {
+      templateUrl: 'templates/views/group_view.html'
+    })
+    .when('/all', {
+      templateUrl: 'templates/views/all_lights_view.html'
+    })
+    .when('/light', {
+      templateUrl: 'templates/views/light_view.html'
+    })
+    .when('/signup', {
+      templateUrl: 'templates/views/auth/auth_view.html'
+    })
+    .when('/login', {
+      templateUrl: 'templates/views/auth/auth_view.html'
+    })
+    .otherwise({
+      redirectTo: '/signup'
+    });
+}]);
