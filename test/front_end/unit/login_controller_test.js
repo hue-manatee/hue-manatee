b7d94ff138575c6ccec32bbe0c1f475b1e785222
Katherine Beame <katherine.beame@gmail.com>
2016-06-06 15:52:47 -0700

    working login controller test

---
+++ test/front_end/unit/login_controller_test.js
@@ -0,0 +1,41 @@
+var angular = require('angular');
+
+describe('LoginController Test', () => {
+  var $httpBackend;
+  var $controller;
+  var loginController;
+
+  beforeEach(angular.mock.module('hueApp'));
+
+  beforeEach(angular.mock.inject((_$controller_) => {
+    $controller = _$controller_;
+  }));
+
+  beforeEach(angular.mock.inject((_$httpBackend_) => {
+    $httpBackend = _$httpBackend_;
+    loginController = $controller('LoginController');
+  }));
+
+  afterEach(() => {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+it('should be a controller', () => {
+  expect(typeof loginController).toBe('object');
+  expect(typeof loginController.authenticate).toBe('function');
+});
+
+it('It should login a user', () => {
+  $httpBackend.expectGET('api/login')
+  .respond(200, { token: 'testResponse' });
+  loginController.user = { username: '', password: '' };
+  loginController.user.password = 'TestPassword123';
+  loginController.user.username = 'TestUser';
+  console.log(loginController.user);
+  loginController.authenticate(loginController.user);
+  $httpBackend.flush();
+  expect(window.localStorage.token).toBe('testResponse');
+});
+
+});
