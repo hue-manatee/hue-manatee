6a66185960d68223db20094d51fe922b876e3fa0
BRANDON PARKER <Brandon@dkparker.com>
2016-06-06 14:23:44 -0700

    user creation test is working

--- test/bundle.js
+++ test/bundle.js
@@ -35402,6 +35402,7 @@
 	describe('Signup Controller test', function() {
 	  var $httpBackend;
 	  var $controller;
+	  var signupController;
 	
 	  beforeEach(angular.mock.module('hueApp'));
 	
@@ -35409,10 +35410,29 @@
 	    $controller = _$controller_;
 	  }));
 	
+	  beforeEach(angular.mock.inject(function(_$httpBackend_) {
+	    $httpBackend = _$httpBackend_;
+	    signupController = $controller('SignUpController');
+	  }));
+	
+	  afterEach(() => {
+	    $httpBackend.verifyNoOutstandingExpectation();
+	    $httpBackend.verifyNoOutstandingRequest();
+	  });
+	
 	  it('should be a controller', function() {
-	    var signupController = $controller('SignUpController');
 	    expect(typeof signupController).toBe('object');
 	    expect(typeof signupController.authenticate).toBe('function');
+	  });
+	
+	  it('should create a user', function() {
+	    $httpBackend.expectPOST('/api/signup', {
+	      username: 'TestMcgee', password: 'SuperTest1'
+	    }).respond(200, { token: 'this is a tolkien' });
+	    signupController.authenticate({ username: 'TestMcgee', password: 'SuperTest1' } );
+	    $httpBackend.flush();
+	    // expect(this.errors.length).toBe(0);
+	    expect(window.localStorage.token).toBe('this is a ');
 	  });
 	});
 
--- test/front_end/unit/auth_controller_test.js
+++ test/front_end/unit/auth_controller_test.js
@@ -3,6 +3,7 @@ var angular = require('angular');
 describe('Signup Controller test', function() {
   var $httpBackend;
   var $controller;
+  var signupController;
 
   beforeEach(angular.mock.module('hueApp'));
 
@@ -10,9 +11,27 @@ describe('Signup Controller test', function() {
     $controller = _$controller_;
   }));
 
+  beforeEach(angular.mock.inject(function(_$httpBackend_) {
+    $httpBackend = _$httpBackend_;
+    signupController = $controller('SignUpController');
+  }));
+
+  afterEach(() => {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
   it('should be a controller', function() {
-    var signupController = $controller('SignUpController');
     expect(typeof signupController).toBe('object');
     expect(typeof signupController.authenticate).toBe('function');
+  });
+
+  it('should create a user', function() {
+    $httpBackend.expectPOST('/api/signup', {
+      username: 'TestMcgee', password: 'SuperTest1'
+    }).respond(200, { token: 'this is a tolkien' });
+    signupController.authenticate({ username: 'TestMcgee', password: 'SuperTest1' } );
+    $httpBackend.flush();
+    expect(window.localStorage.token).toBe('this is a tolkien');
   });
 });
