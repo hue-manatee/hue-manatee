45950b8b459f0d835185eb36c915e1c7e0161f59
CarrieShort <carrie.r.phillips@gmail.com>
2016-06-07 15:48:21 -0700

    finish reset and update functions for all controller closes #202

--- app/js/controllers/all_lights.js
+++ app/js/controllers/all_lights.js
@@ -1,6 +1,43 @@
 module.exports = function(app) {
   app.controller('AllLightsController', ['$http', '$location', function($http, $location) {
     // this.all = [];
+    this.update = function(target, alert, colorLoop) {
+      $http({
+        method: 'GET',
+        url: '/api/light/magic',
+        params: {
+          hex: this.color,
+          lightId: target,
+          on: this.state,
+          bri: this.brightness,
+          alert: alert,
+          effect: colorLoop,
+          group: 'all'
+        },
+        headers: {
+          token: window.localStorage.token
+        }
+      })
+      .then((res) => {
+        console.log('success light change: ', res);
+      }, (response) => {
+        console.log('fail no light change :( ', response);
+      });
+    };
+    this.reset = function() {
+      $http({
+        method: 'GET',
+        url: '/api/light/all/reset',
+        headers: {
+          token: window.localStorage.token
+        }
+      })
+      .then((res) => {
+        console.log('successful reset: ', res);
+      }, (response) => {
+        console.log('reset failed: ', response);
+      });
+    };
     this.getAll = function() {
       $http({
         method: 'GET',
