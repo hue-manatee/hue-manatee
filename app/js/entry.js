06506243c6708cbee26c9bc3aa29867c718a96d7
Katherine Beame <katherine.beame@gmail.com>
2016-06-08 11:41:28 -0700

    have a working group details route

--- app/js/entry.js
+++ app/js/entry.js
@@ -17,10 +17,10 @@ hueApp.config(['$routeProvider', function($routing) {
       controller: 'BridgeController',
       controllerAs: 'bridgeCtrl'
     })
-    .when('/group', {
+    .when('/group/:groupName', {
       templateUrl: 'templates/views/group_view.html',
-      controller: 'GroupController',
-      controllerAs: 'groupCtrl'
+      controller: 'GroupDetailController',
+      controllerAs: 'groupDetailCtrl'
       // TODO: Create a route and view for the group/thisgroupnamething
     })
     .when('/all', {
