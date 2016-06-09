e0ab6a79f5ad9b7b41adf0bffcdec0ad4bd70288
bonecrushereb <bonecrushereb@gmail.com>
2016-06-09 16:18:02 -0700

    logout button functional and placed

--- app/js/controllers/auth/logout.js
+++ app/js/controllers/auth/logout.js
@@ -1,12 +1,15 @@
 module.exports = function(app) {
   app.controller('LogoutController', ['hueAuth', '$location', function(hueAuth, $location) {
     this.logout = function() {
-      if (hueAuth.token) {
-         this.loggedout = true;
-      }
+      // this.reveal = hueAuth.getToken;
       hueAuth.removeToken();
       // TODO: change this path to the choose signup or login page
       $location.path('/');
+    };
+    this.reveal = function() {
+      console.log(hueAuth.getToken(), 'reveal fired');
+      if (hueAuth.getToken()) return true;
+      return false;
     };
   }]);
 };
--- app/js/services/hue_auth.js
+++ app/js/services/hue_auth.js
@@ -2,13 +2,14 @@ module.exports = function(app) {
   app.factory('hueAuth', ['$http', function($http) {
     return {
     getToken: function() {
+      console.log('getToken fired', window.localStorage.token);
       return window.localStorage.token;
     },
     removeToken: function() {
       this.token = null;
       this.username = null;
       $http.defaults.headers.common.token = null;
-      window.localStorage.token = '';
+      window.localStorage.removeItem('token');
     }
   };
   }]);
