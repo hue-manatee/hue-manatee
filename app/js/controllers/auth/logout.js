79b0a5e17f5ab4688393c0dd3a07b7de848e156a
bonecrushereb <bonecrushereb@gmail.com>
2016-06-09 14:55:27 -0700

    logout button postioned

--- app/js/controllers/auth/logout.js
+++ app/js/controllers/auth/logout.js
@@ -1,6 +1,9 @@
 module.exports = function(app) {
   app.controller('LogoutController', ['hueAuth', '$location', function(hueAuth, $location) {
     this.logout = function() {
+      if (hueAuth.token) {
+         this.loggedout = true;
+      }
       hueAuth.removeToken();
       // TODO: change this path to the choose signup or login page
       $location.path('/');
